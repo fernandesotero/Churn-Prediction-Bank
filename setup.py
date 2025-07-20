60615220813f2809a92afaeb7d183b1e79193318
Daniel Fernandes <fernandesotero@proton.me>
2025-07-20 14:06:09 -0300

    Add modelling_utils.py / Some import and errors fixed

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@ Script to build the project as a package
 '''
 
 from setuptools import find_packages, setup
-from typing import List, list
-
+from typing import List
 
 HYPHEN_E_DOT = '-e .'
-def get_requirementes(file_path: str) -> List[str]:
+
+def get_requirements(file_path: str) -> List[str]:
     '''
-        Function to return the list of packages in requirements.txt.
+    Function to return the list of packages in requirements.txt.
 
-        Parameters
-        ----------
-        @param file_path: Path of requirements.txt file [type: string].
+    Parameters
+    ----------
+    @param file_path: Path of requirements.txt file [type: string].
 
-        Return
-        ------
-        @return requirements: List of strings containing the packages for the project.  
-    ''' 
-    requirements = list()
+    Return
+    ------
+    @return requirements: List of strings containing the packages for the project.  
+    '''
+    requirements = []
     with open(file_path) as file:
-        requirements = file.readline()
-        requirements = [req.replace('\n', '') for req in requirements]
+        requirements = file.readlines()
+        requirements = [req.strip() for req in requirements]
 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
@@ -30,12 +30,13 @@ def get_requirementes(file_path: str) -> List[str]:
     return requirements
 
 
-# Defining the setup.
+# Defining the setup
 setup(
-    name = 'Bank Chrun Prediction',
-    version = '0.0.1',
-    author = 'Daniel Fernandes',
-    author_email = 'fernandesotero@proton.me',
-    packages = find_packages(),
-    install_requires = get_requirementes('requirements.txt')
+    name='bank-churn-prediction',
+    version='0.0.1',
+    author='Daniel Fernandes',
+    author_email='fernandesotero@proton.me',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=get_requirements('requirements.txt')
 )
